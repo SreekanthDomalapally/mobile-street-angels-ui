0c823e3794e2494b954d41d01637dcdee964c8aa
Sreekanth Domalapally <sreekanth.domalapally@gmail.com>
2026-06-10 02:03:15 -0700

    logo

--- constants/branding.ts
+++ constants/branding.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'YouHoo Alert';
 export const APP_CAPTION = 'Assistance when you need it most.';
 
-/** Matches the YouHooLogo.jpeg background */
+/** Matches the YouHoo logo navy background */
 export const BRAND_NAVY = '#0B1B32';
 
-export const APP_LOGO = require('@/assets/images/YouHooLogo.jpeg');
+export const APP_LOGO = require('@/assets/images/YouHooLogo.png');
